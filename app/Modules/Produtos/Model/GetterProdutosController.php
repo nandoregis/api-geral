82667a3043a825379ea7e9b1e92af97edafa46aa
Luís Fernando <nando.1regis@gmail.com>
2025-12-10 22:07:27 -0300

    procedimento da api, controller produtos

---
+++ app/Modules/Produtos/Model/GetterProdutosController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\Modules\Produtos\Model;
+
+use app\Model\Model;
+
+class GetterProdutosController extends Model
+{
+
+
+    public function __construct() 
+    {
+        parent::__construct();
+    }
+
+
+    public function getAll()
+    {
+        
+    }
+}
