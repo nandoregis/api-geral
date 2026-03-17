be197499d51a24fcf8484d61242a65bcb538c097
Fernando Regis <nando.1regis@gmail.com>
2026-03-16 21:16:45 -0300

    UUID size 36, seguindo padrões, codigo gerado por IA

--- app/Core/UUID.php
+++ app/Core/UUID.php
@@ -3,15 +3,20 @@
 namespace app\Core;
 
 class UUID
-{
-    public static function generator() {         
-        $bytes = random_bytes(15);
-        return bin2hex($bytes);
+{   
+    
+    /**
+     *  Generate a random UUIDv4.
+     *  @return string UUID
+     */
+    public static function v4()
+    {
+        $data = random_bytes(16);
+
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 
-    public static function generator_apiKey()
-    {
-        $bytes = random_bytes(32);
-        return bin2hex($bytes);
-    }
 }
