66d78b3bf5127440e699e63323ac48f17d83c515
Fernando Regis <nando.1regis@gmail.com>
2026-03-21 22:10:56 -0300

    classe service, criar codigo de barras AN13

---
+++ app/Service/BarcodeGenerator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\Service;
+
+class BarcodeGenerator
+{
+    public static function generateEAN13WithPrefix(string $prefix = '789') : string
+    {
+        $code = $prefix;
+        while(strlen($code) < 12) $code .= random_int(0,9);
+        return $code . self::checkDigit($code);
+    }
+
+    private static function checkDigit(string $code) : int
+    {
+        $sum = 0;
+        for($i = 0; $i < strlen($code); $i++) 
+        {   
+            $digit = (int)$code[$i];
+            $sum += ( $i % 2 === 0) ? $digit  : $digit * 3;
+        }
+
+        return ( 10 - ($sum % 10) ) % 10;
+        
+    }
+}
