e6b0c9844e19610c76812d5e219970e3ba283fdf
Fernando Regis <nando.1regis@gmail.com>
2026-03-22 19:56:35 -0300

    add simbolo contrario

--- app/Modules/Produtos/Validator/ProductValidator.php
+++ app/Modules/Produtos/Validator/ProductValidator.php
@@ -15,7 +15,7 @@ class ProductValidator
         return $this->baseValidate(
             fn() => $this->helpBaseValidate('reference', fn() => Validation::noExist($reference), 'Não está com o parametro reference, faça a correção.'),
             fn() => $this->helpBaseValidate('reference', fn() => Validation::isEmpty($reference), 'Reference não pode ser vazia.'),
-            fn() => $this->helpBaseValidate('reference', fn() => Validation::isString($reference), 'Reference deve ter o tipo string'),
+            fn() => $this->helpBaseValidate('reference', fn() => !Validation::isString($reference), 'Reference deve ter o tipo string'),
             fn() => $this->helpBaseValidate('reference', fn() => !Validation::regex($reference, ProductRules::REFERENCE), 'Referência fora do formato esperado.')
         );
         
@@ -27,7 +27,7 @@ class ProductValidator
         return $this->baseValidate(
             fn() => $this->helpBaseValidate('uuid', fn() => Validation::noExist($uuid), 'Não está com o parametro uuid, faça a correção.'),
             fn() => $this->helpBaseValidate('uuid', fn() => Validation::isEmpty($uuid), 'UUID não foi informado, é obrigatorio!'),
-            fn() => $this->helpBaseValidate('uuid', fn() => Validation::isString($uuid), 'UUID deve ter o tipo string')
+            fn() => $this->helpBaseValidate('uuid', fn() => !Validation::isString($uuid), 'UUID deve ter o tipo string')
         );
 
     }
@@ -38,7 +38,7 @@ class ProductValidator
         return $this->baseValidate(
             fn() => $this->helpBaseValidate('name', fn() => Validation::noExist($name), 'Não está com o parametro name, faça a correção.'),
             fn() => $this->helpBaseValidate('name', fn() => Validation::isEmpty($name), 'Name não pode ser vazio.'),
-            fn() => $this->helpBaseValidate('name', fn() => Validation::isString($name), 'Name deve ter o tipo string'),
+            fn() => $this->helpBaseValidate('name', fn() => !Validation::isString($name), 'Name deve ter o tipo string'),
             fn() => $this->helpBaseValidate('name', fn() => !Validation::regex($name, ProductRules::NAME), 'Name fora do formato esperado')
         );
     }
@@ -49,7 +49,7 @@ class ProductValidator
         return $this->baseValidate(
             fn() => $this->helpBaseValidate('name', fn() => Validation::noExist($name), 'Não está com o parametro name, faça a correção.'),
             fn() => $this->helpBaseValidate('name', fn() => Validation::isEmpty($name), 'name não pode ser vazio.'),
-            fn() => $this->helpBaseValidate('name', fn() => Validation::isString($name), 'name deve ter o tipo string'),
+            fn() => $this->helpBaseValidate('name', fn() => !Validation::isString($name), 'name deve ter o tipo string'),
             fn() => $this->helpBaseValidate('name', fn() => !Validation::regex($name, ProductRules::NAME_SIZE), 'name fora do formato esperado')
         );
 
@@ -61,7 +61,7 @@ class ProductValidator
         return $this->baseValidate(
             fn() => $this->helpBaseValidate('color_hex', fn() => Validation::noExist($color_hex), 'Não está com o parametro color_hex, faça a correção.'),
             fn() => $this->helpBaseValidate('color_hex', fn() => Validation::isEmpty($color_hex), 'color_hex não pode ser vazio.'),
-            fn() => $this->helpBaseValidate('color_hex', fn() => Validation::isString($color_hex), 'clor_hex deve ter o tipo string'),
+            fn() => $this->helpBaseValidate('color_hex', fn() => !Validation::isString($color_hex), 'clor_hex deve ter o tipo string'),
             fn() => $this->helpBaseValidate('color_hex', fn() => !Validation::regex($color_hex, ProductRules::COLOR_HEX), 'color_hex fora do formato esperado, formato esperado #FFFFFF.')
         );
 
@@ -73,7 +73,7 @@ class ProductValidator
         return $this->baseValidate(
             fn() => $this->helpBaseValidate('price', fn() => Validation::noExist($price), 'Não está com o parametro price, faça a correção.'),
             fn() => $this->helpBaseValidate('price', fn() => Validation::isEmpty($price), 'price não pode ser vazio.'),
-            fn() => $this->helpBaseValidate('price', fn() => Validation::isString($price), 'price deve ter o tipo string'),
+            fn() => $this->helpBaseValidate('price', fn() => !Validation::isString($price), 'price deve ter o tipo string'),
             fn() => $this->helpBaseValidate('price', fn() => !Validation::regex($price, ProductRules::PRICE), 'price fora do formato esperado, formato esperado : 0,00')
         );
 
@@ -84,7 +84,7 @@ class ProductValidator
         return $this->baseValidate(
             fn() => $this->helpBaseValidate('quantity', fn() => Validation::noExist($quantity), 'Não está com o parametro quantity, faça a correção.'),
             fn() => $this->helpBaseValidate('quantity', fn() => Validation::isEmpty($quantity), 'quantity não pode ser vazio.'),
-            fn() => $this->helpBaseValidate('quantity', fn() => Validation::isString($quantity), 'quantity deve ter o tipo string'),
+            fn() => $this->helpBaseValidate('quantity', fn() => !Validation::isString($quantity), 'quantity deve ter o tipo string'),
             fn() => $this->helpBaseValidate('quantity', fn() => !Validation::regex($quantity, ProductRules::INT), 'quantity fora do formato esperado, formato esperado inteiro')
         );
     }
